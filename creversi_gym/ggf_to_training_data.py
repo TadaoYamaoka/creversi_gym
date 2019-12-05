8fb7afce1e671c53f5ab1b1ce2fab912363649fc
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-12-05 21:09:16 +0900

    TrainingDataに終端を表すフラグ追加 引き分けの割合を指定するオプション追加

--- creversi_gym/ggf_to_training_data.py
+++ creversi_gym/ggf_to_training_data.py
@@ -8,9 +8,10 @@ from creversi import GGF
 parser = argparse.ArgumentParser()
 parser.add_argument('dir')
 parser.add_argument('training_data')
-parser.add_argument('--rating', type=int, default=0)
-parser.add_argument('--moves', type=int, default=1)
+parser.add_argument('--rating', type=int, default=2000)
+parser.add_argument('--moves', type=int, default=45)
 parser.add_argument('--max_num', type=int, default=100000000)
+parser.add_argument('--draw_ratio', type=float, default=0.1)
 args = parser.parse_args()
 
 th_rating = args.rating
@@ -28,6 +29,8 @@ def find_all_files(directory):
             if os.path.splitext(file)[1] == '.ggf':
                 yield os.path.join(root, file)
 
+win_count = { 'black': 0, 'white': 0, 'draw': 0 }
+draw_ratio = args.draw_ratio
 i = 0
 for path in find_all_files(args.dir):
     print(path)
@@ -38,21 +41,33 @@ for path in find_all_files(args.dir):
         if len(moves) < th_moves:
             continue
 
+        if result == 0 and (win_count['black'] + win_count['white']) * draw_ratio < win_count['draw']:
+            continue
+        if result > 0:
+            win_count['black'] += 1
+        elif result < 0:
+            win_count['white'] += 1
+        else:
+            win_count['draw'] += 1
+
         board.reset()
         for move in moves:
-            if move != PASS:
-                board.to_bitboard(training_data[i]['bitboard'])
-                training_data[i]['turn'] = board.turn
-                training_data[i]['move'] = move
-                if board.turn:
-                    reward = 1 if result > 0 else (-1 if result < 0 else 0)
-                else:
-                    reward = -1 if result > 0 else (1 if result < 0 else 0)
-                training_data[i]['reward'] = reward
-                i += 1
-                assert board.is_legal(move)
+            board.to_bitboard(training_data[i]['bitboard'])
+            training_data[i]['turn'] = board.turn
+            training_data[i]['move'] = move
+            if board.turn:
+                reward = 1 if result > 0 else (-1 if result < 0 else 0)
+            else:
+                reward = -1 if result > 0 else (1 if result < 0 else 0)
+            training_data[i]['reward'] = reward
+            training_data[i]['done'] = False
+            assert board.is_legal(move)
             board.move(move)
+            i += 1
+
+        training_data[i - 1]['done'] = True
 
     print(i)
 
 training_data[:i].tofile(args.training_data)
+print(win_count)
